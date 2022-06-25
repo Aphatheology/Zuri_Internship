a7ec2eec8edb82e27a30c90961fd3df81a0b8cb3
Abdulkareem Mustapha <aphatheology@gmail.com>
2022-06-25 15:03:33 +0100

    First JS Task

---
+++ Basic-JS/script.js
@@ -0,0 +1,7 @@
+let fullName = "Abdulkareem Mustapha"
+let height = 5.7
+let country = "Nigeria"
+// console.log(name)
+// alert(`My name is ${name}, my height is ${height} and I am from ${country}!`)
+
+document.write(`My name is ${fullName}, my height is ${height} and I am from ${country}!`)
