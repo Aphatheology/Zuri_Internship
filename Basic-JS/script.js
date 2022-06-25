545cbfd1e02943eeed8cebbbae592c67e1e82990
Abdulkareem Mustapha <aphatheology@gmail.com>
2022-06-25 15:37:39 +0100

    Changes to method

--- Basic-JS/script.js
+++ Basic-JS/script.js
@@ -1,8 +1,7 @@
 let fullName = "Abdulkareem Mustapha"
 let height = 5.7
 let country = "Nigeria"
-// console.log(name)
-// alert(`My name is ${name}, my height is ${height} and I am from ${country}!`)
 
-// document.write(`My name is ${fullName}, my height is ${height} and I am from ${country}!`)
-document.write(`Name: ${fullName}<br>Height: ${height}<br>Country: ${country}`)
+// document.write(`Name: ${fullName}<br>Height: ${height}<br>Country: ${country}`)
+
+alert(`Name: ${fullName} \nHeight: ${height} \nCountry: ${country}`)
