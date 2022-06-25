9eabf6dbce0fb0934b4e1fbbfbf54120e8fe7a97
Abdulkareem Mustapha <aphatheology@gmail.com>
2022-06-25 15:10:39 +0100

    Second JS Task

--- Basic-JS/script.js
+++ Basic-JS/script.js
@@ -4,4 +4,5 @@ let country = "Nigeria"
 // console.log(name)
 // alert(`My name is ${name}, my height is ${height} and I am from ${country}!`)
 
-document.write(`My name is ${fullName}, my height is ${height} and I am from ${country}!`)
+// document.write(`My name is ${fullName}, my height is ${height} and I am from ${country}!`)
+document.write(`Name: ${fullName}<br>Height: ${height}<br>Country: ${country}`)
---
+++ Calculator/calculator.js
@@ -0,0 +1,63 @@
+let num1, num2, operator, result;
+num1 = +prompt('Enter first number: ');
+num2 = +prompt('Enter second number: ');
+operator = prompt('Enter any of these operator: \n +, -, *, / ');
+// if (operator === '+' || operator === 0) {
+//     result = num1 + num2;
+//   } else if (operator === '-') {
+//     result = num1 - num2;
+//   } else if (operator === '/') {
+//     result = num1 / num2;
+//   } else if (operator === '*') {
+//     result = num1 * num2;
+//   } 
+//   if (num1 === 0 && num2 === 0 && (operator === 0 || operator === '+')) {
+//     alert(`The result of ${num1} plus ${num2} is 0`);
+//   } else {
+//     alert(`The result of ${num1} ${operator} ${num2} is ${result}`);
+//   }
+
+// alert(`You entered ${num1}`);
+// if (num1 != 0) {
+//     operator = prompt('Enter operator (type +, -, / or *):');
+//     if(operator != 0) {
+//         num2 = +prompt('Enter second number:');
+//         if(num2 != 0) {
+//             if (operator === '+') {
+//                 result = num1 + num2;
+//               } else if (operator === '-') {
+//                 result = num1 - num2;
+//               } else if (operator === '/') {
+//                 result = num1 / num2;
+//               } else if (operator === '*') {
+//                 result = num1 * num2;
+//               } else {
+//                 alert('Error! Kindly check the operator used');
+//               }
+//                 alert(`The result is ${result}`);
+//             }
+//     }
+
+// }
+//  operator = prompt('Enter operator (type +, -, / or *):');
+//  num2 = +prompt('Enter second number:');
+
+
+
+if (isNaN(num1) || isNaN(num2)) {
+  alert('Error! Kindly refresh the page and input numbers in the required boxes!');
+} else if (operator === '+') {
+  result = num1 + num2;
+} else if (operator === '-') {
+  result = num1 - num2;
+} else if (operator === '/') {
+  result = num1 / num2;
+} else if (operator === '*') {
+  result = num1 * num2;
+} else {
+  alert('Error! Kindly check the operator used');
+}
+
+let message = num1 + ' ' + operator + ' ' + num2 + ' = ' + result;
+
+alert(message);
